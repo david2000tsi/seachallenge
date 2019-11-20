23c4d2da100929593fbaeeb8a4ea3e9f43149ae2
David Alves <david2000tsi@gmail.com>
2019-11-20 11:42:56 -0200

    Added initial version of application

---
+++ src/test/java/com/app/seachallenge/SeachallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.seachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeachallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
